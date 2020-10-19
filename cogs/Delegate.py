16efeb130bed7dd832f07968dd76c0681e7b20ef
Jason Khan <43140671+khanjason@users.noreply.github.com>
2020-10-19 18:55:56 +0100

    added preamble feature

--- cogs/Delegate.py
+++ cogs/Delegate.py
@@ -31,8 +31,14 @@ class Delegate(commands.Cog):
                     self.general_speakers[ctx.guild.id]=tmp
                     await ctx.channel.send(ctx.author.mention+' has been added to the General Speakers List!')
 
-    
-        
+    @commands.command()
+    async def preamble(self,ctx):
+        if (self.session)[ctx.guild.id]==True:
+            preambs=["Acknowledging", 'Affirming', 'Alarmed', 'Anxious', 'Approving', 'Aware','Bearing in mind', 'Believing', 'Concerned', 'Confident', 'Conscious', 'Considering', 'Convinced', 'Disturbed', 'Determined', 'Emphasizing', 'Encouraged', 'Endorsing', 'Expressing', 'Guided by', 'Having ...adopted', '...approved', '...considered', '...examined further', '...received', '...reviewed', 'Keeping in mind', 'Mindful', 'Noting',
+'...with approval', '...with concern', '...with deep concern', '...with grave concern', '...with regret', '...with satisfaction', 'Observing', 'Reaffirming', 'Realizing', 'Recalling', 'Recognising', 'Regretting', 'Reiterating', 'Seeking', 'Stressing', 'Welcoming']
+            
+            
+            await ctx.channel.send('```Preambulatory phrases: '+preambs+'```')
         
 def setup(bot):
     bot.add_cog(Delegate(bot))
