d48dbe0ab8759af378b28f688b92ab24b3716632
Jason Khan <43140671+khanjason@users.noreply.github.com>
2020-12-31 19:13:33 +0000

    format texr

--- cogs/Delegate.py
+++ cogs/Delegate.py
@@ -128,7 +128,8 @@ class Delegate(commands.Cog):
     @commands.command(brief='Send a note.', description='Send a note by mentioning the recipient followed by the message.')
     async def note(self,ctx,member : discord.Member):
         
-        text=ctx.message.content
+        textli=ctx.message.content.split(' ')
+        text=' '.join(word for word in textli[2:])
         print(text)
         sesstag = self.sessionTable.find_one({"_id":ctx.guild.id})
         sess=sesstag.get("session")
